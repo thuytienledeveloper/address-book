276f12955ef4216a5ba66990def1ec69af71cc48
thuy tien le <thuytienle@thuys-MacBook-Air.local>
2021-03-30 14:32:08 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+import { Table, Container, Navbar, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  FaRegEnvelope,
+  FaPlus,
+  FaUserAlt,
+  FaPhoneAlt,
+  FaTrashAlt
+} from "react-icons/fa";
 
-function App() {
+export default function App() {
+  const [contact, setContact] = useState({
+    name: "",
+    phone: ""
+  });
+  const [contacts, setContacts] = useState([]);
+
+  const addContact = e => {
+    e.preventDefault();
+    const { name, phone } = contact;
+    const formValid =
+      name && /^(\([0-9]{3}\)|[0-9]{3}-)[0-9]{3}-[0-9]{4}$/.test(phone);
+    if (!formValid) {
+      return;
+    }
+    setContacts(contacts => [...contacts, { id: uuidv4(), name, phone }]);
+  };
+
+  const deleteContact = index => {
+    setContacts(contacts => contacts.filter((_, i) => i !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Navbar>
+        <Navbar.Brand href="#home">
+          <FaRegEnvelope className="App-logo" /> My UT Phone Book
+        </Navbar.Brand>
+      </Navbar>
+      <div className="App">
+        <form onSubmit={addContact}>
+          <div className="row pb-5 border border-1 mb-5">
+            <div className=" col-12 text-title pb-3">
+              <h2 className="bg-secondary text-white">Add New Contact</h2>{" "}
+            </div>
+            <div className="col">
+              <input
+                class="form-control"
+                placeholder="Name"
+                value={contact.name}
+                onChange={e =>
+                  setContact(contact => ({ ...contact, name: e.target.value }))
+                }
+              />
+            </div>
+            <div className="col">
+              <input
+                class="form-control"
+                placeholder="Phone Number"
+                value={contact.phone}
+                onChange={e =>
+                  setContact(contact => ({ ...contact, phone: e.target.value }))
+                }
+              />{" "}
+            </div>
+            <div className="col-auto">
+              <Button variant="secondary" type="submit">
+                <FaPlus /> Add
+              </Button>
+            </div>
+          </div>
+          <Table className="table">
+            <tbody>
+              {contacts.map((contact, index) => {
+                return (
+                  <tr key={contact.id}>
+                    <td>
+                      <FaUserAlt />
+                      {contact.name}
+                    </td>
+                    <td>
+                      <FaPhoneAlt />
+                      {contact.phone}
+                    </td>
+                    <td>
+                      <FaTrashAlt onClick={() => deleteContact(index)} />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </form>
+      </div>
+    </Container>
   );
 }
-
-export default App;
